ee89ee6e6ae5580869da3abf9b375ca5b82a5094
Minkaron <aaron.tucaz@gmail.com>
2020-07-13 10:55:25 -0700

    Saving to array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$.ajax({ url: 'https://xbt1ufjtw9.execute-api.us-west-1.amazonaws.com/default/twitterInfo' }).then(
+const tweet = $.ajax({ url: 'https://xbt1ufjtw9.execute-api.us-west-1.amazonaws.com/default/twitterInfo' }).then(
     (data) => {
         console.log(data);
     },
