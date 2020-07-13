ad8a979a19658f8dd130705c6191523fedbe6833
Minkaron <aaron.tucaz@gmail.com>
2020-07-13 11:07:12 -0700

    Fixed variable

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-const tweet = $.ajax({ url: 'https://xbt1ufjtw9.execute-api.us-west-1.amazonaws.com/default/twitterInfo' }).then(
+let tweet;
+
+$.ajax({ url: 'https://xbt1ufjtw9.execute-api.us-west-1.amazonaws.com/default/twitterInfo' }).then(
     (data) => {
-        console.log(data);
+        tweet = data;
     },
     (error) => {
         console.log('Bad Request ' + error);
     }
 );
+
+console.log(tweet);
